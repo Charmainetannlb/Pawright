3e7ef5e48c88f5d7e8f9b3f838c3dfc57c6cfa6c
Charmainetannlb <53883843+Charmainetannlb@users.noreply.github.com>
2024-05-18 14:45:02 +0000

    flowdone

--- script.js
+++ script.js
@@ -11,6 +11,47 @@ document.querySelectorAll('input[name="HDB-approved"]').forEach(function(radio) 
     radio.addEventListener('click', function() {
         //The instruction for these radio buttons is to add a listener event
       document.getElementById("Age1").classList.add('hidden')
+      document.getElementById("character").classList.remove('hidden');
+    });
+  });
+ 
+  document.querySelectorAll('input[class="personality"]').forEach(function(checkbox) {
+    //This grabs all the goddamn buttons called personality
+    checkbox.addEventListener('click', function() {
+      var maxChecked = 2;
+      var checkboxes = document.querySelectorAll('input[class="personality"]');
+      
+        var checkedCount = 0;
+        // Count the number of checkboxes checked
+        checkboxes.forEach(function(checkbox) {
+          if (checkbox.checked) {
+            checkedCount++;
+          }
+        });
+        // If the number of checked checkboxes exceeds the limit, prevent further checks
+        if (checkedCount > maxChecked) {
+          this.checked = false; // Uncheck the checkbox that was just checked
+          alert('You can only check up to ' + maxChecked + ' checkboxes.');
+        }    
+    });
+  });
+
+
+
+  // Get a reference to the button element
+var button = document.getElementById("Next");
+
+// Attach an event listener to the button
+button.addEventListener("click", function() {
+  // Code to execute when the button is clicked
+  document.getElementById("character").classList.add('hidden')
+  document.getElementById("lifestyle").classList.remove('hidden');
+});
+  document.querySelectorAll('input[name="others"]').forEach(function(checkbox) {
+    //This grabs all the goddamn buttons called personality
+    checkbox.addEventListener('click', function() {
+        //The instruction for these checkboxes is to add a listener event
+      document.getElementById("character").classList.add('hidden')
       document.getElementById("xxxxxx").classList.remove('hidden');
     });
   });
