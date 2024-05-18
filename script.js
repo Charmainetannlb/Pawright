1ed1935f41bb7cf3453e3a7deed3721d6130fae5
Charmainetannlb <53883843+Charmainetannlb@users.noreply.github.com>
2024-05-18 09:51:55 +0000

    Radioworks

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.querySelectorAll('input[name="HDB-approved"]').forEach(function(radio) {
+    //This grabs all the goddamn buttons called HDB-approved
     radio.addEventListener('click', function() {
-      /*var targetId = this.id.replace('Radio', '');
-      document.querySelectorAll('.interface').forEach(function(interface) {
-        interface.classList.remove('active');
-      });*/
-      //alert(1);
-      document.getElementById("HDBphoto").classList.add('hidden');
+        //The instruction for these radio buttons is to add a listener event
+      document.getElementById("HDBphoto").classList.add('hidden')
+      document.getElementById("Age1").classList.remove('hidden');
+    });
+  });
+  document.querySelectorAll('input[name="ageDog"]').forEach(function(radio) {
+    //This grabs all the goddamn buttons called ageDog
+    radio.addEventListener('click', function() {
+        //The instruction for these radio buttons is to add a listener event
+      document.getElementById("Age1").classList.add('hidden')
+      document.getElementById("xxxxxx").classList.remove('hidden');
     });
   });
