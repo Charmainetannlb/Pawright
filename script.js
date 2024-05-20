50b18a24844bcbf042a19893804f4223b2b15c3b
Charmainetannlb <53883843+Charmainetannlb@users.noreply.github.com>
2024-05-20 11:02:59 +0000

    Fixfootnote

--- script.js
+++ script.js
@@ -57,11 +57,3 @@ button.addEventListener("click", function() {
       document.getElementById("results").classList.remove('hidden');
     });
   });
-
-
-d3.csv("/data/employees.csv", function(data) {
-    for (var i = 0; i < data.length; i++) {
-        console.log(data[i].Name);
-        console.log(data[i].Age);
-    }
-});
