31edbf77f4e88faa70a228d3129aee6732aa1fd5
Charmainetannlb <53883843+Charmainetannlb@users.noreply.github.com>
2024-05-21 09:52:02 +0000

    Collectionfix

--- script.js
+++ script.js
@@ -1,11 +1,86 @@
-document.querySelectorAll('input[name="HDB-approved"]').forEach(function(radio) {
-    //This grabs all the goddamn buttons called HDB-approved
+/*function collect() {
+  var HDBApproved = document.querySelector('input[name="HDBapproved"]:checked').value;
+  var DOB = document.getElementById("Age1").value;
+  var Active = document.querySelector('input[name="character"]:checked').value;
+  var Calm = document.querySelector('input[name="calm"]:checked').value;
+  var FoodMo = document.querySelector('input[name="foodMo"]:checked').value;
+  var Intelligent = document.querySelector('input[name="intelligent"]:checked').value;
+  var Outgoing = document.querySelector('input[name="outgoing"]:checked').value;
+  var Cat = document.querySelector('input[name="cat"]:checked').value;
+  var Child = document.querySelector('input[name="child"]:checked').value;
+  var DogOther = document.querySelector('input[name="dogOther"]:checked').value;
+
+  console.log("HDBApproved:", HDBApproved);
+  console.log("DOB:", DOB);
+  console.log("Active:", Active);
+  console.log("Calm:", Calm);
+  console.log("FoodMo:", FoodMo);
+  console.log("Intelligent:", Intelligent);
+  console.log("Outgoing:", Outgoing);
+  console.log("Cat:", Cat);
+  console.log("Child:", Child);
+  console.log("DogOther:", DogOther);
+}*/
+
+function collect() {
+  var HDBApprovedInput = document.querySelector('input[name="HDBapproved"]:checked');
+  var DOBInput = document.getElementById("Age1");
+  var ActiveInput = document.querySelector('input[name="Active"]:checked');
+  var CalmInput = document.querySelector('input[name="Calm"]:checked');
+  var FoodMoInput = document.querySelector('input[name="FoodMo"]:checked');
+  var IntelligentInput = document.querySelector('input[name="Intelligent"]:checked');
+  var OutgoingInput = document.querySelector('input[name="Outgoing"]:checked');
+  var CatInput = document.querySelector('input[name="Cat"]:checked');
+  var ChildInput = document.querySelector('input[name="Child"]:checked');
+  var DogOtherInput = document.querySelector('input[name="DogOther"]:checked');
+
+  console.log("HDBApproved input:", HDBApprovedInput);
+  console.log("DOB input:", DOBInput);
+  console.log("Active input:", ActiveInput);
+  console.log("Calm input:", CalmInput);
+  console.log("FoodMo input:", FoodMoInput);
+  console.log("Intelligent input:", IntelligentInput);
+  console.log("Outgoing input:", OutgoingInput);
+  console.log("Cat input:", CatInput);
+  console.log("Child input:", ChildInput);
+  console.log("DogOther input:", DogOtherInput);
+
+  // Now let's try to retrieve the values
+  var HDBApproved = HDBApprovedInput ? HDBApprovedInput.value : null;
+  var DOB = DOBInput ? DOBInput.value : null;
+  var Active = ActiveInput ? ActiveInput.value : null;
+  var Calm = CalmInput ? CalmInput.value : null;
+  var FoodMo = FoodMoInput ? FoodMoInput.value : null;
+  var Intelligent = IntelligentInput ? IntelligentInput.value : null;
+  var Outgoing = OutgoingInput ? OutgoingInput.value : null;
+  var Cat = CatInput ? CatInput.value : null;
+  var Child = ChildInput ? ChildInput.value : null;
+  var DogOther = DogOtherInput ? DogOtherInput.value : null;
+
+  console.log("HDBApproved:", HDBApproved);
+  console.log("DOB:", DOB);
+  console.log("Active:", Active);
+  console.log("Calm:", Calm);
+  console.log("FoodMo:", FoodMo);
+  console.log("Intelligent:", Intelligent);
+  console.log("Outgoing:", Outgoing);
+  console.log("Cat:", Cat);
+  console.log("Child:", Child);
+  console.log("DogOther:", DogOther);
+}
+
+
+document.querySelectorAll('input[name="HDBapproved"]').forEach(function(radio) {
+    //This grabs all the goddamn buttons called HDBapproved
     radio.addEventListener('click', function() {
         //The instruction for these radio buttons is to add a listener event
       document.getElementById("HDBphoto").classList.add('hidden')
       document.getElementById("Age1").classList.remove('hidden');
     });
   });
+
+
+
   document.querySelectorAll('input[name="ageDog"]').forEach(function(radio) {
     //This grabs all the goddamn buttons called ageDog
     radio.addEventListener('click', function() {
@@ -15,6 +90,8 @@ document.querySelectorAll('input[name="HDB-approved"]').forEach(function(radio) 
     });
   });
  
+
+
   document.querySelectorAll('input[class="personality"]').forEach(function(checkbox) {
     //This grabs all the goddamn buttons called personality
     checkbox.addEventListener('click', function() {
@@ -49,11 +126,53 @@ button.addEventListener("click", function() {
 });
 
 var button = document.getElementById("Done");
-document.querySelectorAll('input[name="others"]').forEach(function(checkbox) {
-   // Attach an event listener to the button
-button.addEventListener("click", function() {
+ // Attach an event listener to the button
+ button.addEventListener("click", function() {
+  collect();
+document.querySelectorAll('input[class="others"]').forEach(function(checkbox) {
         //The instruction for these checkboxes is to add a listener event
       document.getElementById("lifestyle").classList.add('hidden')
       document.getElementById("results").classList.remove('hidden');
     });
   });
+
+var database = new Array();
+d3.csv("Data/Doggos17052024 - Sheet1.csv", 
+    function(data) {
+      database.push(data);
+      /*console.log(data.Active);
+      console.log(data.Address);
+      console.log(data.Calm);
+      console.log(data.Cat);
+      console.log(data.Child);
+      console.log(data.DOB);
+      console.log(data.DogOther);
+      console.log(data.FoodMo);
+      console.log(data.Gender);
+      console.log(data.HBDApproved);
+      console.log(data.Holdingshel);
+      console.log(data.ImageLink);
+      console.log(data.Intelligent);
+      console.log(data.Name);
+      console.log(data.Outgoing);*/
+    }
+  );
+console.log(database);
+
+
+
+/*function collect() {
+  var HDBApproved = document.getElementById("HDBphoto").value;
+  var DOB = document.getElementById("Age1").value;
+  var Active = document.getElementById("character").value;
+  var Calm = document.getElementById("character").value;
+  var FoodMo = document.getElementById("character").value;
+  var Intelligent = document.getElementById("character").value;
+  var Outgoing = document.getElementById("character").value;
+  var Cat = document.getElementById("lifestyle").value;
+  var Child = document.getElementById("lifestyle").value;
+  var DogOther = document.getElementById("lifestyle").value;
+  var text = "";
+console.log();''
+}*/
+
